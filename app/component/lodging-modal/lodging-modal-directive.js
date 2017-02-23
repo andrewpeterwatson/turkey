ad1538a4722b8920f2fcc93e27d457b900f41077
Andrew Watson <andrewwatson@Andrew-Watsons-Mac-Pro.local>
2017-02-23 14:54:21 -0800

    added social camping and adjusted tent camping

--- app/component/lodging-modal/lodging-modal-directive.js
+++ app/component/lodging-modal/lodging-modal-directive.js
@@ -23,12 +23,18 @@ angular.module('turkey')
 
     this.currentLodging = 'nothing',
     this.hotel = false;
+    this.tent = false;
+    this.sinle = false;
+    this.share = false;
 
     this.lodgingSelection = function() {
 
       this.currentLodging = lodgeSelectionService.lodgeSelection.lodging;
 
       this.currentLodging === 'hotel' ? this.hotel = true : this.hotel = false;
+      this.currentLodging === 'tent' ? this.tent = true : this.tent = false;
+      this.currentLodging === 'single' ? this.single = true : this.single = false;
+      this.currentLodging === 'share' ? this.share = true : this.share = false;
 
       return
     }
