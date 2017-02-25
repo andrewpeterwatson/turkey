1350249e516422a0bb99b16532ebbbecc0357cef
Andrew Watson <andrewwatson@Andrew-Watsons-Mac-Pro.local>
2017-02-25 13:16:17 -0800

    added els for scedule and fonts

--- app/component/map/map-directive.js
+++ app/component/map/map-directive.js
@@ -13,6 +13,19 @@ angular.module('turkey')
   };
 
     function HomeController($location) {
+
+      function initMap() {
+       var uluru = {lat: -25.363, lng: 131.044};
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 4,
+         center: uluru
+       });
+       var marker = new google.maps.Marker({
+         position: uluru,
+         map: map
+       });
+     }
+
     }
   }
 )
